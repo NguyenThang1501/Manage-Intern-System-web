7eac4c78381569f3e42c0221191ead76bddd787f
NTThang <thang6a1xp@gmail.com>
2023-12-23 22:52:10 +0700

    Update student and teacher pages

--- my-app/src/components/student/StudentView.jsx
+++ my-app/src/components/student/StudentView.jsx
@@ -2,43 +2,50 @@ import React from "react";
 import SideBar from "../common/sidebar/SideBar";
 import Usersymbol from "../user-icon/Usersymbol";
 import "./student.css";
+import Container from "react-bootstrap/esm/Container";
 
 const StudentView = () => {
   return (
     <div className="student-view">
       <SideBar />
-      <Usersymbol />
-      <div className="student-content">
-        <h3 className="heading-student">
-          Hướng dẫn sinh viên đăng ký thực tập
-        </h3>
-        <div className="guide-1">
-          <img
-            className="img-guide-1"
-            src="./images/student-guide.png"
-            alt="student-guide"
-          ></img>
-          <p>
-            Hướng dẫn 1: Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Itaque possimus nesciunt doloremque fugit accusantium tempore,
-            sapiente beatae architecto cupiditate aspernatur ipsam reprehenderit
-            consequatur placeat? Dolorem adipisci repudiandae nihil qui animi?
-          </p>
-        </div>
-        <div className="guide-2">
-          <p>
-            Hướng dẫn 2: Lorem ipsum dolor sit amet consectetur, adipisicing
-            elit. Dolorum veritatis repellat eligendi recusandae doloremque odio
-            fugiat ut consequuntur, explicabo quibusdam reiciendis aliquid a
-            incidunt voluptas deserunt, laboriosam dicta corporis libero.
-          </p>
-          <img
-            className="img-guide-2"
-            src="./images/student-guide-2.png"
-            alt="student-guide-2"
-          />
-        </div>
+      <div className="hi-user">
+        <Usersymbol />
       </div>
+      <Container>
+        <div className="student-content">
+          <h3 className="heading-student">
+            Hướng dẫn sinh viên đăng ký thực tập
+          </h3>
+          <div className="guide-1">
+            <img
+              className="img-guide-1"
+              src="./images/student-guide.png"
+              alt="student-guide"
+            ></img>
+            <p>
+              Hướng dẫn 1: Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Itaque possimus nesciunt doloremque fugit accusantium
+              tempore, sapiente beatae architecto cupiditate aspernatur ipsam
+              reprehenderit consequatur placeat? Dolorem adipisci repudiandae
+              nihil qui animi?
+            </p>
+          </div>
+          <div className="guide-2">
+            <p>
+              Hướng dẫn 2: Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Dolorum veritatis repellat eligendi recusandae doloremque
+              odio fugiat ut consequuntur, explicabo quibusdam reiciendis
+              aliquid a incidunt voluptas deserunt, laboriosam dicta corporis
+              libero.
+            </p>
+            <img
+              className="img-guide-2"
+              src="./images/student-guide-2.png"
+              alt="student-guide-2"
+            />
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
--- my-app/src/components/user-icon/Usersymbol.jsx
+++ my-app/src/components/user-icon/Usersymbol.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import './usersymbol.css'
+import React from "react";
+import "./usersymbol.css";
 import { PiStudentBold } from "react-icons/pi";
+
+import Dropdown from "react-bootstrap/Dropdown";
 const Usersymbol = () => {
   return (
-    <div className='my-user'>
-      <PiStudentBold className='student-icon'/>
+    <Dropdown>
       <div>
-        <li className='user-name'>Hi, Thang</li>
-        <li className='user-role'>Student</li>
+        <Dropdown.Toggle className="my-user">
+          <PiStudentBold className="student-icon" />
+          <div className="user-infor">
+            <li className="user-name">Hi, Thang</li>
+            <li className="user-role">Student</li>
+          </div>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Đăng xuất</Dropdown.Item>
+        </Dropdown.Menu>
       </div>
+    </Dropdown>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Usersymbol
+export default Usersymbol;
