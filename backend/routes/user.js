a3fb9bd34bf7ec0fcb10c551fcf4c4292598a15e
tklee-123 <95230812+tklee-123@users.noreply.github.com>
2023-12-26 15:52:59 +0700

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,26 +5,23 @@ const userController = require("../controllers/userController");
 const router = express.Router();
 
 // Lấy tất cả các hồ sơ (Chỉ Admin có quyền)
-router.get("/teacher/student-mana", middlewareController.verifyTokenAndAdmin, userController.getAllProfiles);
+router.get("/teacher/teacher-mana", middlewareController.verifyTokenAndAdmin, userController.getAllProfiles);
 
 // Lấy hồ sơ người dùng (Admin hoặc người dùng)
-router.get("/teacher/student-mana/:id", middlewareController.verifyToken, userController.getProfile);
+router.get("/teacher/teacher-mana/:id", middlewareController.verifyToken, userController.getProfile);
 
 // Cập nhật hồ sơ người dùng (Admin hoặc người dùng)
-router.put("/teacher/student-mana/:id", middlewareController.verifyToken, userController.updateProfile);
+router.put("/teacher/teacher-mana/:id", middlewareController.verifyToken, userController.updateProfile);
 
 // Tạo hồ sơ cho sinh viên có profile null (Chỉ Admin có quyền)
 router.post("/teacher/student-mana/:id", middlewareController.verifyTokenAndAdmin, userController.createProfile);
 
 // Xoá hồ sơ người dùng (Chỉ Admin có quyền)
-router.delete("/teacher/student-mana/:id", middlewareController.verifyTokenAndAdmin, userController.deleteProfile);
-
-router.get("/teacher/business-mana", middlewareController.verifyTokenAndAdmin, userController.getAllBusinesses);
-
-router.get("/teacher/business-mana/:id", middlewareController.verifyTokenAndAdmin, userController.getOneBusiness);
-
-router.put("/teacher/business-mana/:id", middlewareController.verifyTokenAndAdmin, userController.updateBusiness);
-
-router.delete("/teacher/business-mana/:id", middlewareController.verifyTokenAndAdmin, userController.deleteBusiness);
-
+router.delete("/teacher/teacher-mana/:id", middlewareController.verifyTokenAndAdmin, userController.deleteProfile);
+//lấy thông tin báo cáo thường xuyên
+router.get("/teacher/mana-intern/regular-report", middlewareController.verifyTokenAndAdmin,userController.getRegularReport)
+//lấy thông tin báo cáo thường xuyên - chi tiết
+router.get("/teacher/mana-intern/regular-report/details", middlewareController.verifyTokenAndAdmin,userController.getRegularReport_details)
+//chạy thuật toán matching
+router.post("/runalgorithms", middlewareController.verifyTokenAndAdmin,userController.runcode);
 module.exports = router;
