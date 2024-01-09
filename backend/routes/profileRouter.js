607a7bb78c3dd6d1d3dab0ae05fcd82ac7bccf0d
Huyen <khanhhuyen01122003@gmail.com>
2024-01-09 23:09:57 +0700

    nkh

--- backend/routes/profileRouter.js
+++ backend/routes/profileRouter.js
@@ -4,17 +4,17 @@ const profileController = require("../controllers/profileControllers");
 const router = express.Router();
 
 // Lấy tất cả các hồ sơ (Chỉ Admin có quyền)
-router.get("/teacher/teacher-mana", middlewareController.verifyTokenAndAdmin, profileController.getAllProfiles);
+router.get("/teacher/student-mana", middlewareController.verifyTokenAndAdmin, profileController.getAllProfiles);
 
 // Lấy hồ sơ người dùng (Admin hoặc người dùng)
-router.get("/teacher/teacher-mana/:id", middlewareController.verifyToken, profileController.getProfile);
+router.get("/teacher/student-mana/:id", middlewareController.verifyToken, profileController.getProfile);
 
 // Cập nhật hồ sơ người dùng (Admin hoặc người dùng)
-router.put("/teacher/teacher-mana/:id", middlewareController.verifyToken, profileController.updateProfile);
+router.put("/teacher/student-mana/:id", middlewareController.verifyToken, profileController.updateProfile);
 
 // Tạo hồ sơ cho sinh viên có profile null (Chỉ Admin có quyền)
 router.post("/teacher/student-mana/:id", middlewareController.verifyTokenAndAdmin, profileController.createProfile);
 
 // Xoá hồ sơ người dùng (Chỉ Admin có quyền)
-router.delete("/teacher/teacher-mana/:id", middlewareController.verifyTokenAndAdmin, profileController.deleteProfile);
+router.delete("/teacher/student-mana/:id", middlewareController.verifyTokenAndAdmin, profileController.deleteProfile);
 module.exports = router;
