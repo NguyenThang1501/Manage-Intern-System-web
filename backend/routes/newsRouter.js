ad55cc4c069963a814883714f0db2c488802b3f5
Huyen <khanhhuyen01122003@gmail.com>
2024-01-10 22:42:17 +0700

    ab

--- backend/routes/newsRouter.js
+++ backend/routes/newsRouter.js
@@ -7,12 +7,12 @@ const router = express.Router();
 // thêm tin tuyển dụng
 router.post("/add_news", middlewareController.verifyAdminAndBusiness, newsController.add_news);
 //lấy tất cả tin tuyển dụng
-router.get("/mana-news", newsController.getNews);
+router.get("/mana-news", middlewareController.verifyToken, newsController.getNews);
 //lấy tin tuyển dụng của 1 doanh nghiệp
-router.get("/mana-news/business/:id", newsController.get_news_business);
+router.get("/mana-news/business/:id", middlewareController.verifyToken, newsController.get_news_business);
 //lấy tin tuyển dụng của doanh nghiệp đã đăng nhập
-router.get("/mana-news/self-business", middlewareController.verifyToken, newsController.get_news_selfBusiness);
+router.get("/mana-news/self-business", middlewareController.verifyAdminAndBusiness, newsController.get_news_selfBusiness);
 // lấy tin tuyển dụng - chi tiết
-router.get("/mana-news-details/:id", newsController.getNews_details);
+router.get("/mana-news-details/:id", middlewareController.verifyToken, newsController.getNews_details);
 
 module.exports = router;
