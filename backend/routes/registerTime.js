93d3b6f7ff38e019468960cecf459e6a9a18acf2
Huyen <khanhhuyen01122003@gmail.com>
2024-01-10 11:50:27 +0700

    abc

--- backend/routes/registerTime.js
+++ backend/routes/registerTime.js
@@ -5,4 +5,6 @@ const middlewareController = require("../controllers/middlewareController")
 
 router.post('/teacher/open-register', middlewareController.verifyTokenAndAdmin, registerTimeController.createRegisterTime);
 
+router.put('/teacher/update-register', middlewareController.verifyTokenAndAdmin, registerTimeController.updateRegisterTime)
+
 module.exports = router;
