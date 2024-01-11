ea0f8d9a1c8a156a0558c378186de4381e2c26ae
emilynh <vtdquynh3082003@gmail.com>
2024-01-11 14:36:12 +0700

    Update guideline for student homepage

--- frontend/src/components/student/StudentView.jsx
+++ frontend/src/components/student/StudentView.jsx
@@ -1,85 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import SideBar from "../common/sidebar/SideBar";
 import Usersymbol from "../user-icon/Usersymbol";
-import "./student.css";
 import Container from "react-bootstrap/esm/Container";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../context/UserContext";
-// import studentApi from "../../api/studentApi";
+import HướngDẫnĐăngKý from "./guideline"; // Import your new component
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+// Rest of your code...
 
 const StudentView = () => {
-  const navigate = useNavigate();
-  const { userInfo } = useUser();
-  // const [userName, setUserName] = useState();
-
-  console.log(userInfo);
-  useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      navigate("/login");
-    }
-  }, []);
-
-  // useEffect(() => {
-  //   const fetchStudentInfor = async () => {
-  //     try {
-  //       let response = await studentApi.get(userInfo._id, userInfo.accessToken);
-  //       console.log(response);
-  //       let data = response;
-  //       console.log(data);
-  //       setUserName(data.name);
-  //     } catch (error) {
-  //       console.log("Failed to fetch student infor ", error);
-  //     }
-  //   };
-  //   fetchStudentInfor();
-  // }, []);
+  // ... (remaining code)
 
   return (
     <div className="student-view">
       <SideBar />
-      {/* <div className="hi-user">
-        <Usersymbol
-          onClick={() => {
-            localStorage.removeItem("token");
-            navigate("/login");
-          }}
-          userName={userName}
-          userRole={"student"}
-        />
-      </div> */}
+      {/* Your other components here */}
       <Container>
         <div className="student-content">
           <h3 className="heading-student">
             Hướng dẫn sinh viên đăng ký thực tập
           </h3>
-          <div className="guide-1">
-            <img
-              className="img-guide-1"
-              src="./images/student-guide.png"
-              alt="student-guide"
-            ></img>
-            <p>
-              Hướng dẫn 1: Lorem ipsum dolor sit amet consectetur adipisicing
-              elit. Itaque possimus nesciunt doloremque fugit accusantium
-              tempore, sapiente beatae architecto cupiditate aspernatur ipsam
-              reprehenderit consequatur placeat? Dolorem adipisci repudiandae
-              nihil qui animi?
-            </p>
-          </div>
-          <div className="guide-2">
-            <p>
-              Hướng dẫn 2: Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Dolorum veritatis repellat eligendi recusandae doloremque
-              odio fugiat ut consequuntur, explicabo quibusdam reiciendis
-              aliquid a incidunt voluptas deserunt, laboriosam dicta corporis
-              libero.
-            </p>
-            <img
-              className="img-guide-2"
-              src="./images/student-guide-2.png"
-              alt="student-guide-2"
-            />
-          </div>
+          
+          {/* Insert the new component here */}
+          <HướngDẫnĐăngKý />
+
+          {/* Remaining code... */}
         </div>
       </Container>
     </div>
