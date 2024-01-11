de479a81c7d07d959f66aa031cdf40fa88403b62
emilynh <vtdquynh3082003@gmail.com>
2024-01-11 14:45:41 +0700

    Update style of button in common component nha

--- frontend/src/components/common/button/CustomButton.jsx
+++ frontend/src/components/common/button/CustomButton.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import "./button.css";
+// import React from "react";
+// import "./button.css";
+
+// const CustomButton = ({ buttonText, onClick }) => {
+//   return (
+//     <button onClick={onClick} className="my-button">
+//       {buttonText}
+//     </button>
+//   );
+// };
+
+// export default CustomButton;
+
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const CustomButton = ({ buttonText, onClick }) => {
   return (
-    <button onClick={onClick} className="my-button">
-      {buttonText}
-    </button>
+    <Stack spacing={2} direction="row">
+      <Button variant="contained" onClick={onClick}>
+        {buttonText}
+      </Button>
+    </Stack>
   );
 };
 
